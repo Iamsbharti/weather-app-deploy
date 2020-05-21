f54a0b987472a9e0c5cba26bd6ea80d941e08058
Saurabh <saurabhbharti9@gmail.com>
2020-05-22 01:27:23 +0530

    added styles to search box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="app">
-      
+      <main>
+        <form className="search-box">
+          <input
+            type="text"
+            placeholder="search..."
+            name="search"
+            className="search-bar"
+            onChange={(e) => console.log(e.target.value)}
+          />
+        </form>
+      </main>
     </div>
   );
 }
